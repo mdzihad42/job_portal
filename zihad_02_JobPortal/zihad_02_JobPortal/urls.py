4a865a46bb0e259bd43b510b2b2c3c0431333473
MD ZIHAD HOSSAIN <mdzihad01793561142@gmail.com>
2025-12-28 16:11:28 +0600

    Update urls.py

--- zihad_02_JobPortal/zihad_02_JobPortal/urls.py
+++ zihad_02_JobPortal/zihad_02_JobPortal/urls.py
@@ -3,11 +3,6 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
-from django.conf import settings
-from django.conf.urls.static import static
-from django.views.static import serve
-from django.urls import path, re_path
 from job.views import*
 
 urlpatterns = [
@@ -33,5 +28,3 @@ urlpatterns = [
     path('rejected/<int:applied_id>/',rejected, name='rejected'),
     path('job_search/',job_search,name='job_search'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns+=re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
-urlpatterns+=re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
